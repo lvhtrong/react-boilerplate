b8e310d51d340e363fb7586b12a02a996acf180e
Trong Lu <lvhtrong1793@gmail.com>
2019-08-30 09:36:14 +0700

    Remove default example

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,17 +1,28 @@
 /**
  *
- * App
+ * App.js
  *
  * This component is the skeleton around the actual pages, and should only
  * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
  */
 
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from 'containers/HomePage/Loadable';
+import NotFoundPage from 'containers/NotFoundPage/Loadable';
+
+import GlobalStyle from '../../global-styles';
 
 export default function App() {
   return (
     <div>
-      <h1>React Boilerplate</h1>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+      <GlobalStyle />
     </div>
   );
 }
--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -2,6 +2,7 @@
  * NotFoundPage
  *
  * This is the page we show when the user visits a url that doesn't have a route
+ *
  */
 
 import React from 'react';
@@ -11,10 +12,8 @@ import messages from './messages';
 
 export default function NotFound() {
   return (
-    <article>
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    </article>
+    <h1>
+      <FormattedMessage {...messages.header} />
+    </h1>
   );
 }
